ddbe3215ae3c4dd858863669592748062ce6aab9
Loredana <70909576+loredana-stirbu@users.noreply.github.com>
2020-11-08 20:31:54 +0200

    Update for_strl_3.py

--- for_strl_3.py
+++ for_strl_3.py
@@ -1,5 +1,5 @@
 print("Utilizînd ciclul FOR elaboraţi un program care afişează toate numerele pare, pentru intervalul de la 1 la n ,n-este citit de la tastatură.")
 
 n=int(input("Dati un numar: "))
-for n in range(1,n,2):
+for n in range(2,n+1,2):
     print(n)
